e651f90cec2af240ec69533fac7445a17fe83acc
Tiago Alves Pereira <tiago.alves.pereira@outlook.com>
2021-06-25 19:37:18 -0300

    fourth day

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
-const links = document.querySelectorAll('nav ul li a')
-
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
 for (const element of toggle) {
   element.addEventListener('click', () => {
     nav.classList.toggle('show')
   })
 }
+
+const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
   link.addEventListener('click', () => {
@@ -18,13 +15,14 @@ for (const link of links) {
   })
 }
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY >= navHeight) {
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll')
-  }
-})
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
+  window.scrollY >= navHeight
+    ? header.classList.add('scroll')
+    : header.classList.remove('scroll')
+}
 
 const swiper = new Swiper('.swiper-container', {
   slidesPreview: 1,
@@ -49,6 +47,20 @@ scrollReveal.reveal(
   #aervices header, #services .card,
   #testimonials header, #testimonials .testimonials,
   #contact .text, #contact .links
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  window.scrollY >= 560
+    ? backToTopButton.classList.add('show')
+    : backToTopButton.classList.remove('show')
+}
+
+window.addEventListener('scroll', function () {
+  backToTop()
+  changeHeaderWhenScroll()
+})
